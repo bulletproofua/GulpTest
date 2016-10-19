775fc2a641870de4b528d8696594bf9d0244d927
bulletproofua <yuriy.haydeychuk@gmail.com>
2016-10-19 12:08:13 +0300

    added botstrap, gulp: -rename -cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp        = require('gulp'),
     less        = require('gulp-less'),
     browserSync = require('browser-sync'),
     concat      = require('gulp-concat'),
-    uglify     = require('gulp-uglifyjs');  
+    uglify      = require('gulp-uglifyjs'),
+    cssnano     = require('gulp-cssnano'),
+    rename      = require('gulp-rename');
 //назва строго як в json файлі
 
 //task
@@ -14,7 +16,7 @@ gulp.task('mytask', function(){
 });
 */
 
-gulp.task('less', function(){
+gulp.task('less', function() {
     return gulp.src('app/less/main.less') 
     //  ('!app/less/main.less') щоб виключити цей файл з вибірки
     //  (['!app/less/main.less','app/less/*.less']) усі файли лесс крім main.less
@@ -23,15 +25,27 @@ gulp.task('less', function(){
     .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("scripts", function(){
-   return gulp.src('app/libs/jquery/dest/jquery.min.js') 
-       // [ , , , ] можна перечислити усі бібліотеки через ,   
-   .pipe(concat('libs.min.js'))
-   .pipe(uglify())
-   .pipe(gulp.dest('app/js')); 
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+    ])
+    // [ , , , ] можна перечислити усі бібліотеки через ,   
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
 });
 
-gulp.task('browser-sync', function(){
+
+gulp.task('css-libs', ['less'], function(){
+    return gulp.src('app/css/libs.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('browser-sync', function() {
    browserSync({
        server: {
            baseDir: 'app'
@@ -41,7 +55,7 @@ gulp.task('browser-sync', function(){
 });
 
 //таск для слідкування за змінами файлів
-gulp.task('watch', ['browser-sync', 'less'] ,function(){ // [ ] - виконується до того як виконається основна функція
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'] ,function(){ // [ ] - виконується до того як виконається основна функція
     gulp.watch('app/less/**/*.less', ['less']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
